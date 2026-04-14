5a4876e8ec15046ba91b15811e76c1d2354be7d9
Nicholas Keandre <78301853+nicholaskeandre-ai@users.noreply.github.com>
2026-04-14 08:31:32 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int N;
+    printf("Masukkan N: ");
+    scanf("%d", &N);
+
+    printf("\n");
+    for (int i = 1; i <= N; i++) {
+        if ((i % 2 == 0) && (i % 3 == 0)) {
+            printf("BIRU");
+        } else if (i % 3 == 0) {
+            printf("MERAH");
+        } else if (i % 2 == 0) {
+            printf("KUNING");
+        } else if ((i >= 10) && (i % 2 == 0)) {
+            printf("HIJAU");
+        } else {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
